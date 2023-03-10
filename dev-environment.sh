cc5a5569eb625bd318079acc42a50783ca2b5853
p-urple <alex.wasylik@gmail.com>
2023-03-10 08:28:19 -0500

    split script into two versions

--- dev-environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tmux \
-    new-session  -s "test" -n "git" 'vim misc/nginx.conf'\; \
-    split-window -h \; \
-    resize-pane -R 35 \; \
-    new-window -n "servers" 'ssh -t matchbox.local "cd /var/dev ; sudo su ; zsh --login"' \; \
-    split-window -h 'ssh -t matchbox.local "cd /var/www ; sudo su ; zsh --login"' \; \
-    detach-client
