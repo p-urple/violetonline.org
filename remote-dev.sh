52d7486b3abaf3ad922ee86e1dc7af5733e063ab
p-urple <alex.wasylik@gmail.com>
2023-03-10 08:29:10 -0500

    forgot to add files

---
+++ remote-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tmux \
+    new-session  -s "violeonline" -n "git" 'vim misc/nginx.conf'\; \
+    split-window -h \; \
+    resize-pane -R 35 \; \
+    new-window -n "servers" 'ssh -t matchbox.local "cd /var/dev/violetonline.org ; sudo su ; zsh --login"' \; \
+    split-window -h 'ssh -t matchbox.local "cd /var/www/violetonline.org ; sudo su ; zsh --login"' \; \
+    detach-client
