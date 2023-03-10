b40b8df04df4da0cd3a84600d44fdacb79d734e9
p-urple <alex.wasylik@gmail.com>
2023-03-10 10:34:06 -0500

    added extra panel for non-root user in servers window

--- remote-dev.sh
+++ remote-dev.sh
@@ -4,6 +4,7 @@ tmux \
     new-session  -s "violetonline" -n "git" 'vim misc/nginx.conf'\; \
     split-window -h \; \
     resize-pane -R 35 \; \
-    new-window -n "servers" 'ssh -t matchbox.local "cd /var/dev/violetonline.org ; sudo su ; zsh --login"' \; \
+    new-window -n "servers" 'ssh -t matchbox.local' \; \
+    split-window -h 'ssh -t matchbox.local "cd /var/dev/violetonline.org ; sudo su ; zsh --login"' \; \
     split-window -h 'ssh -t matchbox.local "cd /var/www/violetonline.org ; sudo su ; zsh --login"' \; \
     detach-client
